c83235c7e19c84e8d021ac998db660f9a947ab6b
Peter Beattie <peterbeattie19@gmail.com>
2018-02-18 15:42:39 +0000

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node {
+  public int data; 
+  public char c;
+
+  public Node left;
+  public Node right; 
+
+  public Node(int n, char c){
+    this.data = n;
+    this.c = c;
+  }
+
+  public Node(){
+  	
+  }
+
+}
